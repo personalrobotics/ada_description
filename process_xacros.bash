6d4ec0961e41addbe4bbe0812ecb125be34d0514
Ethan Gordon <ekgordon@cs.washington.edu>
2020-12-23 16:54:33 -0800

    Generated URDF and SRDF

--- process_xacros.bash
+++ process_xacros.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-
-xacro --inorder -o robots_urdf/ada.urdf robots/j2n6s200_standalone.xacro
-xacro --inorder -o robots_urdf/ada_with_camera.urdf robots/j2n6s200_standalone.xacro with_camera:=true
-xacro --inorder -o robots_urdf/ada_with_forque.urdf robots/j2n6s200_standalone.xacro with_forque:=true
-xacro --inorder -o robots_urdf/ada_with_camera_forque.urdf robots/j2n6s200_standalone.xacro with_camera:=true with_forque:=true
+echo "Processing ada.urdf..."
+xacro -o urdf/ada.urdf robots/gen3_robotiq_2f_85.xacro
